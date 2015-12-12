f5749940a4847386acf50d8b928c94bef8262efa
Mihai Bercu <cumbaia97@yahoo.co.uk>
2015-12-12 18:06:47 +0000

    login button switching activities

--- app/src/main/java/com/hackkings/wannaplay/Login.java
+++ app/src/main/java/com/hackkings/wannaplay/Login.java
@@ -1,15 +1,18 @@
 package com.hackkings.wannaplay;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 public class Login extends AppCompatActivity {
 
     private EditText usernameField;
     private EditText passwordField;
+    private Button loginButton;
 
     public void signUpOrLogin(View view) {
 
@@ -25,5 +28,13 @@ public class Login extends AppCompatActivity {
 
         usernameField = (EditText) findViewById(R.id.username);
         passwordField = (EditText) findViewById(R.id.password);
+        loginButton = (Button) findViewById(R.id.login_button);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), getEvent.class);
+                startActivity(intent);
+            }
+        });
     }
 }
